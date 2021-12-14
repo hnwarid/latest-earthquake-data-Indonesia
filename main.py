6fe0fa03a0091c5f43cce12733864cfa9b2e03a2
Hamzah Nurwarid <hn.warid@gmail.com>
2021-12-14 17:06:58 +0700

    Update main.py

--- main.py
+++ main.py
@@ -6,5 +6,8 @@ import latest_earthquake
 
 if __name__ == "__main__":
     print("Main Application\n")
-    result = latest_earthquake.data_extraction()
-    latest_earthquake.display_data(result)
+    # result = latest_earthquake.data_extraction()
+    # latest_earthquake.display_data(result)
+    earthquake_in_indonesia = latest_earthquake.LatestEarthquakeNews()
+    earthquake_in_indonesia.data_extraction()
+    earthquake_in_indonesia.display_data()
